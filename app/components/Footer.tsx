9f05840036d8cf442ec22a19ae33d814c200087e
mogy_h <rapter.mg@gmail.com>
2026-09-13 16:20:42 +0900

    HEader, FooterコンポーネントとCSSモジュールの追加

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.copyright}>© 2026 TaskApp</p>
+    </footer>
+  );
+};
