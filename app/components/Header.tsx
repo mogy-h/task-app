2cb954cb9f61a048445b9899e8facd9685be36ec
mogy_h <rapter.mg@gmail.com>
2026-09-13 16:47:07 +0900

    Headerにナビゲーションの追加とスタイリング

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,23 @@
-import styles from './Header.module.css';
+import Link from "next/link";
+import styles from "./Header.module.css";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>TaskApp</h1>
+      <Link href="/" className={styles.logo}>
+        <h1 className={styles.title}>TaskApp</h1>
+      </Link>
+      <nav className={styles.nav}>
+        <Link href="/tasks" className={styles.navLink}>
+          タスク一覧
+        </Link>
+        <Link href="/tasks/new" className={styles.navLink}>
+          タスク作成
+        </Link>
+        <Link href="/settings" className={styles.navLink}>
+          設定
+        </Link>
+      </nav>
     </header>
   );
 };
