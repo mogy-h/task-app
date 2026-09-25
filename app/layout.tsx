f72b3cbc165c62aa29b543f92a148103cd992104
mogy_h <rapter.mg@gmail.com>
2026-09-25 22:10:27 +0900

    feature: shadcn/uiを導入、button, card, input, labelコンポーネントを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata = {
   title: "TaskApp",
@@ -13,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ja">
+    <html lang="ja" className={cn("font-sans", geist.variable)}>
       <body>
         <Header />
         <main>{children}</main>
